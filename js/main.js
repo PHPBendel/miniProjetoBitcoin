016cad493a1a56c6c21c3d4f2ff7c7b6d525f6c8
Pedro Bendel <pedro.bendel@gmail.com>
2017-09-13 17:09:31 -0300

    Add files via upload
    versão com novo json

--- js/main.js
+++ js/main.js
@@ -1,21 +1,57 @@
-$(document).ready(function(){
+$(document).ready(function() {
+
+    var sn = SchemeNumber;
+    var fn = sn.fn;
 
 
-//Faz a chamada pra o proxy, que retorna o JSON
-$.getJSON('proxy.php', function(data) {
+    //Faz a chamada pra o proxy, que retorna o JSON
+    $.getJSON('proxy.php', function(data) {
 
-    var xbt = data.best_offer["xbt-brl"]; //Seleciona a propriedade no JSON
-    
-    //Faz a formatação para mostrar o valor correto
-    //Segundo valor / Primeiro valor
-    var div = xbt.split("/"); 
-    var bitcoins  = parseFloat(div[1]) / parseFloat(div[0]);              
+        var MAX_BTC = new SchemeNumber('1');
 
-    //Faz o tratamento para mostrar o valor no formato de moeda correto
-    var num = 'R$ ' + bitcoins.toFixed(2).replace('.', ',').replace(/(\d)(?=(\d{3})+\,)/g, "$1.");  
+        var page;
+        var qntsPaginas = data.length;
+        var arrayOfResults = data;
 
-    //Passa o valor para o  input
-    $('#bitcoin').val(num);
-});
+
+        var totalOfBitcoins = new SchemeNumber('0');
+        var totalOfReais = new SchemeNumber('0');
+        var foundPrice = false;
+
+        for (page = 0; page < qntsPaginas && !foundPrice; page++) {
+            var sell = arrayOfResults[page];
+
+
+            for (var c = 0; c < sell.length &&
+                fn['<'](totalOfBitcoins, MAX_BTC); c++) {
+
+                var bitcoin = new SchemeNumber(sell[c][0]);
+                var reais = new SchemeNumber(sell[c][1]);
+                var newBitcoinTotal = fn['+'](totalOfBitcoins, bitcoin);
+
+
+                if (fn['<='](newBitcoinTotal, MAX_BTC)) {
+                    totalOfBitcoins = newBitcoinTotal;
+                    totalOfReais = fn['+'](totalOfReais, reais);
+                } else {
+                    var bitcoinToConsider = fn['-'](bitcoin, (fn['-'](newBitcoinTotal, MAX_BTC)));
+                    var pricePerBitcoin = fn['/'](reais, bitcoin);
+                    var reaisToConsider = fn['*'](bitcoinToConsider, pricePerBitcoin);
+                    totalOfBitcoins = fn['+'](totalOfBitcoins, bitcoinToConsider);
+                    totalOfReais = fn['+'](totalOfReais, reaisToConsider);
+
+                    foundPrice = fn['/'](totalOfReais, totalOfBitcoins).toFixed(2);
+                }
+            }
+        }
+
+
+        if (foundPrice) {
+            var finalValue = (parseFloat(foundPrice)).toFixed(2).replace('.', ',').replace(/(\d)(?=(\d{3})+\,)/g, "$1.");
+            $('#bitcoin').val(finalValue);
+        }
+
+    });
+
 
 });
--- proxy.php
+++ proxy.php
@@ -2,9 +2,21 @@
 header('Content-Type: application/json');
 
 	//Pega o JSON da url apontada
-
-    $url = 'https://s3.amazonaws.com/data-production-walltime-info/production/dynamic/meta.json';   
+   	$url = 'https://s3.amazonaws.com/data-production-walltime-info/production/dynamic/meta.json';   
     $json = file_get_contents($url);
-    echo $json;    
+    $data = json_decode($json);
+
+    $pages = $data->order_book_pages;
+    $url = "https://s3.amazonaws.com/data-production-walltime-info/production/dynamic/".$data->order_book_prefix."_r".$data->current_round."_p";
+    $arrayObj = array();
+
+    for ($i = 0; $i<$pages; $i++)
+    {
+        $json = file_get_contents($url.$i.".json");
+        $data = json_decode($json);   
+        array_push($arrayObj, $data->{"xbt-brl"});                       
+    }
+
+    echo json_encode($arrayObj);   
 ?>
 
